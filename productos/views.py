c1e94e25868c4ed7a185d929bd8d978d80ca4578
Julián Laterza <laterzajulian@gmail.com>
2024-05-25 02:03:28 -0300

    update_v2

--- productos/views.py
+++ productos/views.py
@@ -7,9 +7,15 @@ def index(request):
     
     producto = Producto.objects.all()
     #Con HttpResponse es la mejor opción.
-    print(producto)
-    return HttpResponse(producto[0].nombre)
+    #print(producto)
+    #return HttpResponse(producto[0].nombre)
 
     #Con JSONResponse se usa cuando estamos a las apuradas y necesitamos ir a Producción.
     #producto = Producto.objects.all().values()
     #return JsonResponse(list(producto), safe=False)
+
+    return render(
+        request,
+        'index.html',
+        context={'productos':producto}
+    )
